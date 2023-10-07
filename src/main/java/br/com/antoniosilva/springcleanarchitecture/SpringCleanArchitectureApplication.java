dcb99dd0517e90ab1df0fed80eb4cff433cdbd94
Antonio <antoniopsilva083@gmail.com>
2023-10-07 02:30:39 -0300

    start project

---
+++ src/main/java/br/com/antoniosilva/springcleanarchitecture/SpringCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package br.com.antoniosilva.springcleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCleanArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/antoniosilva/springcleanarchitecture/SpringCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antoniosilva.springcleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
